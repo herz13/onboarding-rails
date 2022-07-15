7d6d937c6b9c48d08b01da8747a5e2c34c724547
Mariana <msacra90@gmail.com>
2022-07-15 18:16:34 +0100

    Mapping new array from results

--- app/jobs/fetch_data_job.rb
+++ app/jobs/fetch_data_job.rb
@@ -1,7 +1,17 @@
 class FetchDataJob < ApplicationJob
-  repeat 'every day at 8am'
+  # repeat 'every day at 8am'
 
-  def perform(*args)
-    # Do something later
+  def self.perform(*args)
+    base_url = "http://universities.hipolabs.com"
+    
+    response = HTTParty.get(base_url + "/search?country")
+    new_array = []
+    parsed_response = JSON.parse response.parsed_response.to_json, symbolize_names: true
+
+    parsed_response.each do |obj|
+      new_array << {name: obj[:name], country: obj[:country], alpha_two_code: obj[:alpha_two_code]}
+    end 
+
+    puts new_array
   end
 end
